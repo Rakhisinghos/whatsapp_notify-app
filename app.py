2e340196b5142939478e1ec8f4d2d89729b3202d
Rakhisinghos <rakhisinghos2003@gmail.com>
2025-07-20 23:12:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ Chhattisgarh Institute of Technology (CGIT), Jagdalpur"""
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
